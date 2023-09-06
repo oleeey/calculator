58314ddea7a9700a9015bcdc126516d8f5e1f9d5
Oliver Giczi <oleeeyminecraft@gmail.com>
2023-09-06 21:37:26 +0200

    .

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-    for (let i = 1; i < 10; i++) {
-        $("#calcNums").append(`<p class="nums">${i}</p>`)
+    let nums = [7,8,9,4,5,6,1,2,3]
+    for (i in nums) {
+        $("#calcNums").append(`<p class="nums">${nums[i]}</p>`)
     }
-})
+
+    $(".nums").hover(function() {
+        $(this).addClass("hover");
+    }, function() {
+        $(this).removeClass("hover");
+    })
+});
