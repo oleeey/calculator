6fc295025c20f2566e0a3546a1955bb99c092c3e
Oliver Giczi <oleeeyminecraft@gmail.com>
2023-09-11 20:51:56 +0200

    added operations, started decimal system

--- script.js
+++ script.js
@@ -9,23 +9,59 @@ $(document).ready(function() {
         else {
             input.push(event.target.value);
         }
-        
     })
 
     $("#equals").click(function() {
-        let num = 0;
+        sortInput(input);
+        
+       
         let op = "";
+        //console.log(input)
         for (let i in input) {
+            //console.log(input[i])
             if (typeof(input[i]) == "number") {
-                num = input[i];
+                if (op) {
+                    if (op == "+") {output += input[i];}
+                    if (op == "-") {output -= input[i];}
+                    if (op == "x") {output *= input[i];}
+                    if (op == "/") {input[i] > 0 ? output /= input[i] : output = 0;}
+                    op = "";
+                }
+                else {
+                    output = input[i];
+                }  
             }
             else {
                 op = input[i]
             }
         
         }
-        console.log(num, op)
 
         $("#display").text(output);
     })
+
+    $("#clear").click(function() {
+        input = [];
+        output = 0;
+        $("#display").text(output);
+    })
+
+
 });
+
+function sortInput(list) {
+    for (let i in list) {
+        if (list[i] == list[i - 1]) {
+            list[i] = Number(String(list[i]) + String(list[i - 1]));
+            delete(list[i - 1]);
+        }
+    }
+    console.log(list);
+    for (let i in list) {
+        if (list[i] == ".") {
+            console.log(list[i - 1], list[i], list[i + 1])
+            list[i - 1] += list[i + 1];
+        }
+    }
+    console.log(list)
+}
